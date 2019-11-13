f93035688157db114f1e7194f8642decfb185aec
Jake Maldonado <jmaldonado@financeit.io>
2019-11-13 08:35:38 -0500

    answer 5 and add q 6

--- chapter1/exercise1-5.scm
+++ chapter1/exercise1-5.scm
@@ -13,3 +13,12 @@
 ;What behavior will ben observe with an interpreter that uses applicative order evaluation?
 ;With normal-order evaluation? (Assume the exaluation rule for the spceial form if is the same. Predicate first.)
 
+;applicative order:
+;(How scheme works, all procedures are evaluated when the procedure is applied)
+;applicative order will execute all of the arguments passed in so `p` will be executed and is recursive so it will cause an infinite loop,
+;the function will never complete.
+
+;normal-order
+;(Delays evaluation of procedure arguments until the actual argument values are needed)
+;normal order would return 0 and never end up evaluating y since it was never needed.
+
