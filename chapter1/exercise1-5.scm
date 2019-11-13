090c7bc8c6e3561b456ace3f3947a49951810dc5
Jake Maldonado <jmaldonado@financeit.io>
2019-11-13 09:18:44 -0500

    update q5 notes

--- chapter1/exercise1-5.scm
+++ chapter1/exercise1-5.scm
@@ -1,8 +1,8 @@
-;Ben Bitdiffle has invented a test to determine whether the interpreter he is faced with is using applicative-order
-;evaluation or normal order evalueation. He defines the following two procedures:
+;Ben Bitdiddle has invented a test to determine whether the interpreter he is faced with is using applicative-order
+;evaluation or normal order evaluation. He defines the following two procedures:
 (define (p) (p))
 
-(define (tests x y)
+(define (test x y)
   (if (= x 0)
     0
     y))
@@ -13,12 +13,13 @@
 ;What behavior will ben observe with an interpreter that uses applicative order evaluation?
 ;With normal-order evaluation? (Assume the exaluation rule for the spceial form if is the same. Predicate first.)
 
-;applicative order:
+;APPLICATIVE ORDER:
 ;(How scheme works, all procedures are evaluated when the procedure is applied)
 ;applicative order will execute all of the arguments passed in so `p` will be executed and is recursive so it will cause an infinite loop,
-;the function will never complete.
+;the function will never complete. This seems normal, in most languages that I have used, the return value of the called function being
+;passed in would be the parameter. This is similar to test(0, p()); in javascript.
 
-;normal-order
+;NORMAL-ORDER:
 ;(Delays evaluation of procedure arguments until the actual argument values are needed)
 ;normal order would return 0 and never end up evaluating y since it was never needed.
 
