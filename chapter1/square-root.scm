1707b00630427365dce82fb306b27149659fec5f
Jake Maldonado <jmaldonado@financeit.io>
2019-11-18 09:07:17 -0500

    add square root notes from lecture 1

--- chapter1/square-root.scm
+++ chapter1/square-root.scm
@@ -1,30 +1,36 @@
-;Newtons method:
-; - orginally developed by Heron of Alexandria in the first century A.D.
-; - find the square root by averaging `y` with `x/y`
+; Newtons method:
+;   - orginally developed by Heron of Alexandria in the first century A.D.
+;   - find the square root by averaging `y` with `x/y`
 ;
-;example
+; example
 ;
-;guess: 1
-;quotient: 2/1 = 2
-;average: (2+1)/2 = 1.5
+; guess: 1
+; quotient: 2/1 = 2
+; average: (2+1)/2 = 1.5
 
-;second iteration
-;guess: 1.5
-;quotient: 2/1.5 = 1.3333
-;average: (1.3333+1.5)/2 = 1.4142
+; second iteration
+; guess: 1.5
+; quotient: 2/1.5 = 1.3333
+; average: (1.3333+1.5)/2 = 1.4142
 
-;basic sqrt guessing function using Newtons method
+; using this method, each iteration will bring us closer to the true square root.
+; until we decide we're close enough (it may be impossible to get to the true number so we get something that is very close)
+
+; basic sqrt guessing function using Newtons method
+; we don't need loops since we can do this recursively(we wont use loops for this whole course)
+; this definition is a 'recursive definition' (the ability to make recursive definitions allows you to do infinite computations
+; until somthing goes through, without needing any other constructs other than calling a procedure)
 (define (sqrt-iter guess x)
   (if (good-enough? guess x)
     guess
     (sqrt-iter (improve guess x)
                x)))
 
-;improve the guess by averaging the quotient of the radicand and the old guess
+; improve the guess by averaging the quotient of the radicand and the old guess
 (define (improve guess x)
   (average guess (/ x guess)))
 
-;where
+; where
 
 (define (average x y)
   (/ (+ x y) 2))
@@ -37,5 +43,18 @@
 ; dividing integers might produce a rational number and we want decimals
 (define (sqrt x)
   (sqrt-iter 1.0 x))
+
+; we can define square root using block structure, so that we don't get the system confused with our methods
+; we package our procedures inside of our sqrt procedure.
+(define (sqrt x)
+  (define (improve guess) (average guess (/ x guess)))
+  (define (good-enough? guess)
+    (< (abs (- (square guess) x))
+       .001))
+  (define (try guess)
+    (if (good-enough? guess)
+      guess
+      (try (improve guess))))
+  (try 1))
 
 ; we will need to bring in our other math functions in order for square and abs to work
