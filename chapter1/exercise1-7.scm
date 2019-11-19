1fefb2c7f00d610dfe0365aa867195fb1ec40660
Jake Maldonado <jmaldonado@financeit.io>
2019-11-19 08:33:09 -0500

    answer 1.7

--- chapter1/exercise1-7.scm
+++ chapter1/exercise1-7.scm
@@ -41,14 +41,36 @@
 ; Small numbers:
 ; There are a few reasons why our funtion may not be suitable for small numbers. The first being that the limits that we put on our result
 ; in the `good-enough?` can make our estimate pretty inacurate.
+; actually the good enough difference seems ok. This method seems better for small numbers than large
+; because the large numbers are hitting 100000000 and returning that
 
 
 ; Large Numbers:
-;
+; with large numbers it seems to be getting stuck at 100000000
 
 
 ; An alternative for implementing good-enough? is to watch how guess changes from one iteration to the next and to stop when the change
 ; is a very small fraction of the guess. Design a square-root procedure that uses this kind of end test. Does it work better for small and large numbers?
 
-(define (new-good-enough?)
-  ())
+; now we pass in our guess and our next guess and our next guess and compare them
+(define (sqrt-two x)
+  (define (improve guess) (average guess (/ x guess)))
+  (define (good-enough? guess prev)
+    (< (abs (- (square guess) prev))
+       0.001))
+  (define (try guess prev)
+    (if (good-enough? guess prev)
+      guess
+      (try (improve guess) x)))
+  (try 1.0 2.0))
+
+; the new method seems less accurate for small numbers since the answer for the first one
+; is returned as scientific notation, it's a smaller number and that would seem more accurate.
+
+; for big numbers it seems to be getting stuck at the same point as the original method (100000000)
+
+(sqrt 9999999999999999)
+(sqrt 0.000000000001)
+(sqrt-two 9999999999999999)
+(sqrt-two 0.00000000001)
+
