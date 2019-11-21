82a3c5bb363dd9ad1fc26e075625f78ebbe1c319
Jake Maldonado <jmaldonado@financeit.io>
2019-11-21 08:09:53 -0500

    answer q8

--- chapter1/exercise1-8.scm
+++ chapter1/exercise1-8.scm
@@ -5,32 +5,40 @@
 ; alalogous: similar
 ; note: the type of recursion we are doing here is a `tail call` since we arent calling the outer function again
 ;   this is not to be used in modern JS currently since its not supported in a lot of browsers and not in node.
-(define (cube-root x)
-  (define (improve guess prev)
-    (/ (+ (/ prev
-             (square guess))
-          (* 2 guess))
-       3))
-  (define (good-enough? guess prev)
-    (< (abs (- (square guess) prev))
-       0.001))
-  (define (try guess prev)
-    (if (good-enough? guess prev)
-      guess
-      (try (improve guess prev) x)))
-  (try 1.0 2.0))
 
-(cube-root 27)
-
-; "import" our pre requisite functions
+; our pre requisite functions
 
 (define (average x y)
   (/ (+ x y) 2))
 
 (define (square x) (* x x))
 
+(define (cube x) (* x x x))
+
 (define (abs x)
   (cond ((< x 0)(- x))
         (else x)))
+
+; solution
+
+(define (cube-root x)
+  (define (improve guess)
+    (/ (+ (/ x
+             (square guess))
+          (* 2.0 guess))
+       3.0))
+  (define (good-enough? guess)
+    (< (abs (- (cube guess) x))
+       0.001))
+  (define (try guess)
+    (if (good-enough? guess)
+      guess
+      (try (improve guess))))
+  (try 1.0))
+
+(cube-root 27)
+(cube-root 64)
+(cube-root 343)
+
 
 
