84f20f3a7d32df6f91cf7a286459d3c4efa1ffe5
Jake Maldonado <jmaldonado@financeit.io>
2019-11-26 09:06:24 -0500

    work on q 11

--- chapter1/exercise1-11.scm
+++ chapter1/exercise1-11.scm
@@ -4,8 +4,57 @@
 ; by means of an iterative process.
 
 ; Recursive
+(define (fnc n)
+  (cond ((< n 3) n)
+        ((>= n 3) (+ (fnc (- n 1))
+                    (* 2
+                       (fnc (- n 2)))
+                    (* 3
+                       (fnc (- n 3)))))))
 
+(fnc 3)
 
 
 ; Iterative
+
+; (define (fnc2 n)
+;  (cond ((< n 3) n)
+;        ((>= n 3) (+ (fnc2 (- n 1))
+;                    (* 2
+;                       (fnc2 (- n 2)))
+;                    (* 3
+;                       (fnc2 (- n 3)))))))
+;
+;(define (fnc2-iter a b n)
+;  (if (< n 3)
+;      n
+;      (fnc2-iter (-))))
+;
+;(fnc2 3)
+
+; Static
+
+(define (fnc3 n)
+  (expt (+ (- n 1)
+           ((* 2
+               (- n 2))
+            (* 3
+               (- n 3))))
+        n))
+
+(fnc3 3)
+
+
+;f(n) = ((n - 1) + 2(n - 2) + 3(n - 3))^n
+
+;f(2) =  ((2 - 1) + 2(2 - 2) + 3(2 - 3))^2
+;f(2) =  ((1) + 2(0) + 3(-1))^2
+;f(2) =  (1 - 3)^2
+;f(2) =  (-2)^2
+;f(2) =  (4)^2
+
+
+;f(3) =  ((3 - 1) + 2(3 - 2) + 3(3 - 3))^3
+;f(3) =  (2 + 2)^3
+;46 WRONG should be 4
 
